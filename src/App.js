74eacfad6c9217dcfc29697d1eb5800ba0ae1661
Wilson Ng <wilsonng234@gmail.com>
2023-04-17 00:44:42 +0800

    Create search bar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import { useState, useEffect } from "react";
+import SearchIcon from "@mui/icons-material/Search";
+import IconButton from "@mui/material/IconButton";
 
 import logo from "./logo.svg";
 import "./App.css";
 import getAllDocuments from "./api/getAllDocuments.js";
-
-import AppDocumentCards from "./App.styles";
+import { AppTextField, AppDocumentCards, AppSearchIcon } from "./App.styles";
 
 function App() {
     const [documents, setDocuments] = useState([]);
+    const [query, setQuery] = useState("");
 
     useEffect(() => {
         const fetchDocuments = async () => {
@@ -18,10 +20,33 @@ function App() {
         fetchDocuments();
     }, []);
 
+    const handleKeyDown = (event) => {
+        if (event.key === "Enter") {
+            setQuery(event.target.value);
+        }
+    };
+
+    const handleSearchIconClick = () => {
+        setQuery(document.getElementById("search-bar").value);
+    };
+
     return (
         <div className="App">
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
+                <AppTextField
+                    id="search-bar"
+                    label="Enter your query"
+                    InputProps={{
+                        endAdornment: (
+                            <IconButton
+                                children={<SearchIcon />}
+                                onClick={handleSearchIconClick}
+                            />
+                        ),
+                    }}
+                    onKeyDown={handleKeyDown}
+                />
                 <AppDocumentCards documents={documents} />
             </header>
         </div>
--- src/App.styles.js
+++ src/App.styles.js
@@ -1,8 +1,25 @@
 import styled from "@emotion/styled";
+import { TextField } from "@mui/material";
 import DocumentCards from "./components/document/documentCards";
+import SearchIcon from "@mui/icons-material/Search";
+
+const AppTextField = styled(TextField)({
+    marginBottom: "20px",
+    width: "60%",
+
+    "& .MuiOutlinedInput-root": {
+        borderRadius: "40px",
+    },
+});
 
 const AppDocumentCards = styled(DocumentCards)({
     width: "80%",
 });
 
-export default AppDocumentCards;
+const AppSearchIcon = styled(SearchIcon)({
+    "&: hover": {
+        cursor: "pointer",
+    },
+});
+
+export { AppTextField, AppDocumentCards, AppSearchIcon };
