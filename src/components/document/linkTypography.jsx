f1c3e2adb6fd5bb171a4ac136e440d8595ce0e3d
Wilson Ng <wilsonng234@gmail.com>
2023-04-17 00:59:24 +0800

    Wrap if link overflow

--- src/components/document/linkTypography.jsx
+++ src/components/document/linkTypography.jsx
@@ -2,7 +2,7 @@ import Link from "@mui/material/Link";
 import Typography from "@mui/material/Typography";
 
 const LinkTypography = ({ url }) => (
-    <Typography variant="h6">
+    <Typography variant="h6" style={{ wordBreak: "break-all" }}>
         <Link href={url} underline="none">
             {url}
         </Link>
