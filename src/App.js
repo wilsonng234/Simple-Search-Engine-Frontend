a5f3220831d74edc489c9beafbaca3f4ac8c99f6
Wilson Ng <wilsonng234@gmail.com>
2023-04-25 03:40:30 +0800

    Add Loading button when loading query

--- src/App.js
+++ src/App.js
@@ -6,10 +6,12 @@ import getAllDocuments from "./api/getAllDocuments.js";
 import getDocumentsByQuery from "./api/getDocumentsByQuery";
 import { AppDocumentCards } from "./App.styles";
 import SearchBar from "./components/search-bar/searchBar";
+import ReactLoading from "react-loading";
 
 function App() {
     const [documents, setDocuments] = React.useState([]);
     const [query, setQuery] = React.useState("");
+    const [showLoading, setShowLoading] = React.useState(false);
 
     React.useEffect(() => {
         const fetchDocuments = async () => {
@@ -22,10 +24,11 @@ function App() {
 
     React.useEffect(() => {
         const fetchDocumentsByQuery = async () => {
+            setShowLoading(true);
             const response = await getDocumentsByQuery(query);
             setDocuments(response.data);
+            setShowLoading(false);
         };
-
         fetchDocumentsByQuery();
     }, [query]);
 
@@ -33,11 +36,15 @@ function App() {
         <div className="App">
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
+
                 <SearchBar setQuery={setQuery} />
-                <AppDocumentCards documents={documents} />
+                {showLoading && (
+                    <ReactLoading type={"bars"} color="#0080FF" />
+                )}
+                {!showLoading && <AppDocumentCards documents={documents} />}
             </header>
         </div>
     );
-}
+}   
 
 export default App;
