42d28331791df6b148471e6093afc3a5851c76d0
Wilson Ng <wilsonng234@gmail.com>
2023-05-05 20:00:16 +0800

    Create SearchBarAutocomplete

--- src/components/search-bar/searchBar.jsx
+++ src/components/search-bar/searchBar.jsx
@@ -1,12 +1,19 @@
 import React from "react";
+
+import MenuItem from "@mui/material/MenuItem";
 import SearchIcon from "@mui/icons-material/Search";
 import IconButton from "@mui/material/IconButton";
+import TextField from "@mui/material/TextField";
 
-import SearchBarTextField from "./searchBar.styles";
+import SearchBarAutocomplete from "./searchBar.styles";
 
 const SearchBar = ({ setQuery }) => {
     // This is only for query display purposes
     const [queryDisplayed, setQueryDisplayed] = React.useState("");
+    const [recommendations, setRecommendations] = React.useState([
+        "test",
+        "test page",
+    ]);
 
     const handleOnChange = (event) => {
         setQueryDisplayed(event.target.value);
@@ -14,30 +21,45 @@ const SearchBar = ({ setQuery }) => {
 
     const handleKeyDown = (event) => {
         if (event.key === "Enter") {
-            setQuery(event.target.value);
+            console.log(queryDisplayed);
+            setQuery(queryDisplayed);
         }
     };
 
     const handleSearchIconClick = () => {
-        setQuery(document.getElementById("search-bar").value);
+        console.log(queryDisplayed);
+        setQuery(queryDisplayed);
+    };
+
+    const handleRenderInput = (params) => {
+        return (
+            <TextField
+                {...params}
+                id="search-bar-textfield"
+                label="Enter your query"
+                onChange={handleOnChange}
+                onKeyDown={handleKeyDown}
+                InputProps={{
+                    ...params.InputProps,
+                    endAdornment: (
+                        <IconButton
+                            children={<SearchIcon />}
+                            onClick={handleSearchIconClick}
+                        />
+                    ),
+                }}
+            ></TextField>
+        );
     };
 
     return (
-        <SearchBarTextField
-            id="search-bar"
-            label="Enter your query"
-            InputProps={{
-                endAdornment: (
-                    <IconButton
-                        children={<SearchIcon />}
-                        onClick={handleSearchIconClick}
-                    />
-                ),
-            }}
-            onChange={handleOnChange}
-            onKeyDown={handleKeyDown}
-            value={queryDisplayed}
-        />
+        <SearchBarAutocomplete
+            id="search-bar-autocomplete"
+            freeSolo
+            disableClearable
+            options={recommendations}
+            renderInput={handleRenderInput}
+        ></SearchBarAutocomplete>
     );
 };
 
